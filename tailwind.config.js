2ea719196224a6977b13fbfa7c5b381b966593ad
Bharat <bharatcoder9@gmail.com>
2023-09-24 12:27:31 +0530

    Adding daisyUI and Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    content: ['./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'],
+    plugins: [require("@tailwindcss/typography"), require("daisyui")],
+    theme: {
+      extend: {
+        fontFamily: {
+          //sans: ['Inclusive Sans', ...defaultTheme.fontFamily.sans],
+        },
+      },
+    },
+    daisyui: {
+      themes: ["light"],
+    },
+  };
+  
